7d2c6ebfdc8b4f8116c5dd23d944e0d10aae7665
326826436 <326826436@qq.com>
2022-03-08 22:37:46 +0800

    _2055前缀和

---
+++ src/com/company/normal/_2055.java
@@ -0,0 +1,58 @@
+package com.company.normal;
+
+public class _2055 {
+    public static void main(String[] args) {
+        int[][] queries = {{2, 5}, {5, 9}};
+        String s = "**|**|*********||||||******|";
+        int ans[] = platesBetweenCandles(s, queries);
+        for (int i = 0; i < ans.length; i++) {
+            System.out.println(ans[i]);
+        }
+    }
+
+    public static int[] platesBetweenCandles(String s, int[][] queries) {
+        int len = queries.length;
+        char[] s1 = s.toCharArray();
+        int[] ans = new int[len];
+        int[] firstLeft = new int[s.length()];
+        int[] firstRight = new int[s.length()];
+        int[] preSum = new int[s.length()];
+        int temp = -1;
+        for (int i = 0; i < s.length(); ++i) {
+            if ('|' == s1[i]) {
+                temp = i;
+                firstLeft[i] = i;
+            } else {
+                firstLeft[i] = temp;
+            }
+            if (i != 0) {
+                if ('*' == s1[i]) {
+                    preSum[i] = preSum[i - 1] + 1;
+                } else {
+                    preSum[i] = preSum[i - 1];
+                }
+            }
+        }
+        temp = -1;
+        for (int i = s.length() - 1; i >= 0; --i) {
+            if ('|' == s1[i]) {
+                temp = i;
+            }
+            firstRight[i] = temp;
+        }
+        for (int i = 0; i < len; i++) {
+            int left = firstRight[queries[i][0]];
+            // 得到右侧最近的蜡烛下标
+            int right = firstLeft[queries[i][1]];
+            // 处理特殊情况
+            if (left == -1 || right == -1 || left > right) {
+                ans[i] = 0;
+            } else {
+                // 前缀和相减
+                ans[i] = preSum[right] - preSum[left];
+
+            }
+        }
+        return ans;
+    }
+}
