e0bd5d72e311364a07ebbd2555b0dabf915cc143
326826436@qq.com <326826436@qq.com>
2022-08-07 23:28:03 +0800

    636

---
+++ src/com/company/normal/_636.java
@@ -0,0 +1,24 @@
+package com.company.normal;
+
+import java.util.List;
+
+public class _636 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int[] exclusiveTime(int n, List<String> logs) {
+        int[] ans = new int[n];
+        int len =logs.size();
+        for (int i = 1; i < len; i++) {
+            String[] lastTemp = logs.get(i-1).split(":");
+            String[] temp = logs.get(i).split(":");
+            ans[Integer.parseInt(lastTemp[0])]= temp[Integer.parseInt(temp[2])]
+
+        }
+
+
+        return ans;
+    }
+}
