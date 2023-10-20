839f51e3b09fac57b3a22f686a0b33d4cf2ce582
xuyiquan <326826436@qq.com>
2023-10-20 13:21:02 +0800

    null

--- src/com/company/test/sortTest.java
+++ src/com/company/test/sortTest.java
@@ -1,24 +1,53 @@
 package com.company.test;
 
+import java.text.DecimalFormat;
+
 public class sortTest {
 
     public static void main(String[] args) {
-        int[] a={87,212,3,44,55,33,6,6,9};
-        int[] b =popSort(a);
+        int[] a = {87, 212, 3, 44, 55, 33, 6, 6, 9};
+        int[] b = popSort(a);
         for (int i = 0; i < b.length; i++) {
-            System.out.print(b[i]+" ");
+            System.out.print(b[i] + " ");
         }
+        System.out.println();
+
+        String mPrice = "0000.5611100000";
+        DecimalFormat df = new DecimalFormat("0.00");
+        mPrice = df.format(Double.parseDouble(mPrice)); //保留两位小数
+        System.out.println(mPrice);
+        double pi=3.1415927;//圆周率
+        //取一位整数
+        System.out.println(new DecimalFormat("0").format(pi));//3
+        //取两位小数
+        System.out.println(new DecimalFormat("0.00").format(pi));//3.14
+        //取两位整数和三位小数，整数不足部分以0填补。
+        System.out.println(new DecimalFormat("00.000").format(pi));//03.142
+        //取所有整数部分
+        System.out.println(new DecimalFormat("#").format(pi));//3
+        //以百分比方式计数，并取两位小数
+        System.out.println(new DecimalFormat("#.##%").format(pi));//314.16%
+
+        long c=299792458;//光速
+        //显示为科学计数法，并取五位小数
+        System.out.println(new DecimalFormat("#.#####E0").format(c));//2.99792E8
+        //显示为两位整数的科学计数法，并取四位小数
+        System.out.println(new DecimalFormat("00.####E0").format(c));//29.9792E7
+        //每三位以逗号进行分隔。
+        System.out.println(new DecimalFormat(",###").format(c));//299,792,458
+        //将格式嵌入文本
+        System.out.println(new DecimalFormat("光速大小为每秒,###米").format(c)); //光速大小为每秒299,792,458米
     }
 
 
-    public static int[] popSort(int [] nums){
-        int len =nums.length;
-        for (int i = len; i >0; i--) {
+    public static int[] popSort(int[] nums) {
+        int len = nums.length;
+        for (int i = len; i > 0; i--) {
             for (int j = 1; j < i; j++) {
-                if(nums[j]<nums[j-1]){
-                    int temp =nums[j];
-                    nums[j]=nums[j-1];
-                    nums[j-1]=temp;
+                if (nums[j] < nums[j - 1]) {
+                    int temp = nums[j];
+                    nums[j] = nums[j - 1];
+                    nums[j - 1] = temp;
                 }
             }
         }
